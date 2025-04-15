b3a1c47568637f70655e26b879e952bd83fb88c4
SHINJWOO <judy9150@naver.com>
2025-04-15 03:56:19 -0400

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#include <fstream>
+#include <cstdio>
 #include <cstdint>
 #include <netinet/in.h> // ntohl을 위해 필요
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cerr << "사용법: " << argv[0] << " <파일1> [파일2>..." << "\n";
+        std::cerr << "사용법: " << argv[0] << " <파일1> [파일2]...\n";
         return 1;
     }
 
     uint32_t total = 0;
 
     for (int i = 1; i < argc; ++i) {
-        std::ifstream file(argv[i], std::ios::binary);
+        FILE* file = fopen(argv[i], "rb");
         if (!file) {
             std::cerr << "파일을 열 수 없습니다: " << argv[i] << "\n";
             return 1;
         }
 
         uint32_t num_network;
-        file.read(reinterpret_cast<char*>(&num_network), sizeof(num_network));
+        size_t result = fread(&num_network, sizeof(num_network), 1, file);
+        fclose(file);
 
-        if (file.gcount() != sizeof(num_network)) {
-            std::cerr << "잘못된 파일 크기 " << argv[i] << "\n";
+        if (result != 1) {
+            std::cerr << "잘못된 파일 크기" << argv[i] << "\n";
             return 1;
         }
 
